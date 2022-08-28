a2aa8b06bd84fee4597d4d5f76ee253fb9300744
Teodorooo <tesquerreh@gmail.com>
2022-08-28 15:45:02 -0500

    hhehj

--- gameone.py
+++ gameone.py
@@ -1,12 +1,14 @@
-# Juego de adivinazas
+# Juego de adivinazas mejorado
 import random
 intento = 0 
 var = random.randint(1,10)
 while intento < 4:  
     var1 = int(input("ingrese un numero entre 1 y 10: "))
     intento = intento + 1
-    if var1 != var:
-        print("nada sigue")
+    if var1 + 1 == var or var1 - 1 == var:
+        print("casi lo atinas, CALIENTE!")
+    if var1 != var and (var - 1) > var1 or (var + 1) < var1:
+        print("estas FRIO!")
     if var1 == var:
         print("adivinastes el numero")
         break
---
+++ nose.py
@@ -0,0 +1,21 @@
+# nums = [1, 5, 3, 6, 4, 1, 7]
+# def secentaysiete(nums):
+#     sum = 0
+#     count = True
+#     for i in range(0, len(nums), 1):
+#         if nums[i] == 6:
+#             count = False
+#         elif count == False and nums[i] == 7:
+#             count = True
+#         elif count == True:
+#             sum = sum + nums[i]
+#     return sum
+# print(secentaysiete(nums))
+
+# nums = [1, 2, 3]
+# def has22(nums):
+#     for i in range(0, len(nums) -1 , 1):
+#         if nums[i] == 2 and nums[i + 1] == 2:
+#             return True
+#     return False
+# print(has22(nums))
