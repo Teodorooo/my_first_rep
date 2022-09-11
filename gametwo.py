ad0b352dac60c2a088b5c2060942680a3d15e008
Teodorooo <tesquerreh@gmail.com>
2022-09-11 23:37:28 +0200

    Geografos version 2

--- gametwo.py
+++ gametwo.py
@@ -1,25 +1,20 @@
-# Juego para los geógrafos
+# Juego Geografos no tengo tildes. Version 2.0
 
+import random
+
+P = [["¿ Oceano mas grande del Mundo ?: ", "Pacifico"], ["¿ Cual es la capital de Francia ?: ", "Paris"], ["¿ Cual es el pais que tiene Berlin como su capital ?: ", "Alemania"], ["¿ Cual es la selva mas grande del mundo ?: ", "La Amazonia"], ["¿ Capital de Irlanda del Norte?: ", "Belfast"]]
+random.shuffle(P)
 a = 0
-x = input("¿ Cual es la capital de Francia ?: ")
-if x == "Paris" or x == "paris":
-    print("¡ CORRECTO !")
-    a = a + 1
-elif x != "Paris" or x != "paris":
-    print("Respuesta incorrecta")
-montaña = input("¿ Cual es el pais que tiene Berlin como su capital ?: ")
-if montaña == "Alemania" or montaña == "alemania":
-    print("¡ CORRECTO !")
-    a = a + 1
-elif montaña != "Alemania" or montaña != "alemania":
-    print("Respuesta incorecta")
-amazonas = input("¿ Cual es la selva mas grande del mundo ?: ")
-if amazonas == "La Amazonia" or amazonas == "la Amazonia":
-    print("¡ CORRECTO !")
-    a = a + 1 
-elif amazonas != "La Amazonia" or amazonas != "la Amazonia":
-    print("Respuesta incorrecta")
-print("Tuvistes " + str(a) + " buena[s] respuesta[s]")
+b = 0
+for i in range(0, len(P), 1):
+    user_input = input(P[i][0])
+    if user_input.lower() == P[i][1].lower():
+        print("¡ CORRECTO !")
+        a = a + 1
+    elif user_input.lower() != P[i][1].lower():
+        print("Respuesta incorrecta")
+        b = b + 1
+    print(str(a) + " buena[s] respuesta[s] y " + str(b) + " mala[s]" )
 
     
 
